8425759a58cafd6639a937aab3778e4c85517b8b
vnchen <81728056+vnchen@users.noreply.github.com>
2024-02-27 21:44:19 -0800

    Update model.py

--- model.py
+++ model.py
@@ -11,76 +11,76 @@ from transformers import BertModel, BertConfig
 from transformers import AdamW, get_cosine_schedule_with_warmup, get_linear_schedule_with_warmup
 
 class IntentModel(nn.Module):
-  def __init__(self, args, tokenizer, target_size):
-    super().__init__()
-    self.tokenizer = tokenizer
-    self.model_setup(args)
-    self.target_size = target_size
+    def __init__(self, args, tokenizer, target_size):
+        super().__init__()
+        self.tokenizer = tokenizer
+        self.model_setup(args)
+        self.target_size = target_size
 
-    # task1: add necessary class variables as you wish.
-    
-    # task2: initilize the dropout and classify layers
-    self.dropout = nn.Dropout(args.drop_rate)
-    self.classify = Classifier(args, target_size)
-    
-  def model_setup(self, args):
-    print(f"Setting up {args.model} model")
+        # task1: add necessary class variables as you wish.
 
-    # task1: get a pretrained model of 'bert-base-uncased'
-    self.encoder = BertModel.from_pretrained('bert-base-uncased')
+        # task2: initilize the dropout and classify layers
+        self.dropout = nn.Dropout(args.drop_rate)
+        self.classify = Classifier(args, target_size)
     
-    self.encoder.resize_token_embeddings(len(self.tokenizer))  # transformer_check
+    def model_setup(self, args):
+        print(f"Setting up {args.model} model")
 
-  def forward(self, inputs, targets):
-    """
-    task1: 
-        feeding the input to the encoder, 
-    task2: 
-        take the last_hidden_state's <CLS> token as output of the
-        encoder, feed it to a drop_out layer with the preset dropout rate in the argparse argument, 
-    task3:
-        feed the output of the dropout layer to the Classifier which is provided for you.
-    """
-    outputs = self.encoder(**inputs)
-    hidden = outputs.last_hidden_state[:, 0, :]
-    hidden = self.dropout(hidden)
-    logit = self.classify(hidden)
-    return logit
-  
+        # task1: get a pretrained model of 'bert-base-uncased'
+        self.encoder = BertModel.from_pretrained('bert-base-uncased')
+
+        self.encoder.resize_token_embeddings(len(self.tokenizer))  # transformer_check
+
+    def forward(self, inputs, targets):
+        """
+        task1: 
+            feeding the input to the encoder, 
+        task2: 
+            take the last_hidden_state's <CLS> token as output of the
+            encoder, feed it to a drop_out layer with the preset dropout rate in the argparse argument, 
+        task3:
+            feed the output of the dropout layer to the Classifier which is provided for you.
+        """
+        outputs = self.encoder(**inputs)
+        hidden = outputs.last_hidden_state[:, 0, :]
+        hidden = self.dropout(hidden)
+        logit = self.classify(hidden)
+        return logit
+
 class Classifier(nn.Module):
-  def __init__(self, args, target_size):
-    super().__init__()
-    input_dim = args.embed_dim
-    self.top = nn.Linear(input_dim, args.hidden_dim)
-    self.relu = nn.ReLU()
-    self.bottom = nn.Linear(args.hidden_dim, target_size)
+    def __init__(self, args, target_size):
+        super().__init__()
+        input_dim = args.embed_dim
+        self.top = nn.Linear(input_dim, args.hidden_dim)
+        self.relu = nn.ReLU()
+        self.bottom = nn.Linear(args.hidden_dim, target_size)
 
-  def forward(self, hidden):
-    middle = self.relu(self.top(hidden))
-    logit = self.bottom(middle)
-    return logit
+    def forward(self, hidden):
+        middle = self.relu(self.top(hidden))
+        logit = self.bottom(middle)
+        return logit
 
 
 class CustomModel(IntentModel):
-  def __init__(self, args, tokenizer, target_size):
-    super().__init__(args, tokenizer, target_size)
+    def __init__(self, args, tokenizer, target_size):
+        super().__init__(args, tokenizer, target_size)
     
     # task1: use initialization for setting different strategies/techniques to better fine-tune the BERT model
 
 class SupConModel(IntentModel):
-  def __init__(self, args, tokenizer, target_size, feat_dim=768):
-    super().__init__(args, tokenizer, target_size)
+    def __init__(self, args, tokenizer, target_size, feat_dim=768):
+        super().__init__(args, tokenizer, target_size)
 
     # task1: initialize a linear head layer
  
-  def forward(self, inputs, targets):
+    #def forward(self, inputs, targets):
 
-    """
-    task1: 
-        feeding the input to the encoder, 
-    task2: 
-        take the last_hidden_state's <CLS> token as output of the
-        encoder, feed it to a drop_out layer with the preset dropout rate in the argparse argument, 
-    task3:
-        feed the normalized output of the dropout layer to the linear head layer; return the embedding
-    """
+    #"""
+   # task1: 
+    #    feeding the input to the encoder, 
+    #task2: 
+    #    take the last_hidden_state's <CLS> token as output of the
+    #    encoder, feed it to a drop_out layer with the preset dropout rate in the argparse argument, 
+    #task3:
+    #    feed the normalized output of the dropout layer to the linear head layer; return the embedding
+    #"""
